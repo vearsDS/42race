03e8520288c29808e2f5783e898e1f8cc2bdb353
vearsds <andreasgustaviano@gmail.com>
2024-06-16 17:21:10 +0700

    new change

--- controller/stravaController.js
+++ controller/stravaController.js
@@ -2,34 +2,36 @@ import { client, clients } from "../config/mongodb.js";
 import request from 'request'
 
 
-const getRefreshToken = async (clientId, client_secret, refreshToken) => {
+const getRefreshToken = async (client_id, client_secret, refresh_token) => {
     try {
         const clientss = await clients('Accounts');
         await clientss.findOne({ client_id: '128672' }, (err, result) => {
             if (err) {
                 throw err;
             }
-            if (!result) {
-                clientss.insertOne({
-                    client_id: "128672",
-                    client_secret: '291d4656589fab4a77307a7a71392283b6ca7aa4',
-                    refresh_token: responseBody.refresh_token
-                });
-
-            }
         })
-        request.post(`https://www.strava.com/oauth/token?client_id=128672&client_secret=291d4656589fab4a77307a7a71392283b6ca7aa4&code=${code}&grant_type=refresh_token`, (error, response, body) => {
-
+        request.post(`https://www.strava.com/oauth/token?client_id=128672&client_secret=291d4656589fab4a77307a7a71392283b6ca7aa4&code=${code}&grant_type=refresh_token`, async (error, response, body) => {
+            const responseBody = JSON.parse(response.body)
+            await clientss.insertOne({
+                client_id,
+                client_secret,
+                refresh_token,
+                access_token,
+                expires_at,
+                expires_in
+            });
         });
     } catch (err) {
         return new Error(err)
     }
-}
+};
+
+
 export const getStraveAPI = async (req, res) => {
     try {
         const { code } = req.query;
         const clientss = await clients('Accounts');
-        let client_id, client_secret, refreshToken
+        let client_id, client_secret, refresh_token, access_token, expires_at, expires_in
         const existing = await clientss.findOne({ client_id: '128672' }, (err, result) => {
             if (err) {
                 throw err;
@@ -39,7 +41,14 @@ export const getStraveAPI = async (req, res) => {
         if (existing) {
             client_id = result.client_id;
             client_secret = result.client_secret;
-            refreshToken = result.refreshToken;
+            refresh_token = result.refresh_token;
+            access_token = result.access_token;
+            expires_at = result.expires_at;
+            expires_in = request.expires_in;
+            if (expires_at < Date.now() / 1000) {
+                await getRefreshToken('128672', '291d4656589fab4a77307a7a71392283b6ca7aa4', refresh_token)
+            }
+
         } else {
             request.post(`https://www.strava.com/oauth/token?client_id=128672&client_secret=291d4656589fab4a77307a7a71392283b6ca7aa4&code=${code}&grant_type=authorization_code`, async (error, response, body) => {
                 const responseBody = JSON.parse(response.body)
@@ -49,13 +58,23 @@ export const getStraveAPI = async (req, res) => {
                 } else {
                     client_id = responseBody.client_id;
                     client_secret = responseBody.client_secret;
-                    refreshToken = responseBody.refreshToken;
+                    refresh_token = responseBody.refresh_token;
+                    access_token = responseBody.access_token;
+                    expires_in = responseBody.expires_in
+                    await clientss.insertOne({
+                        client_id: client_id,
+                        client_secret,
+                        refresh_token,
+                        access_token,
+                        expires_at,
+                        expires_in
+                    });
                 }
 
             });
         }
 
-        return res.status(200).json(code)
+        return res.status(200).json(`Successfully Login`)
     } catch (err) {
         return res.status(200).json({ success: true, message: "router doesn't works" })
     } finally {
