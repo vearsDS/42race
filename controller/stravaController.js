1196778329913112d6fa6facb838ac0f93cb71c7
vearsds <andreasgustaviano@gmail.com>
2024-06-16 17:03:45 +0700

    new change

--- controller/stravaController.js
+++ controller/stravaController.js
@@ -1,12 +1,61 @@
 import { client, clients } from "../config/mongodb.js";
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import request from 'request'
 
+
+const getRefreshToken = async (clientId, client_secret, refreshToken) => {
+    try {
+        const clientss = await clients('Accounts');
+        await clientss.findOne({ client_id: '128672' }, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            if (!result) {
+                clientss.insertOne({
+                    client_id: "128672",
+                    client_secret: '291d4656589fab4a77307a7a71392283b6ca7aa4',
+                    refresh_token: responseBody.refresh_token
+                });
+
+            }
+        })
+        request.post(`https://www.strava.com/oauth/token?client_id=128672&client_secret=291d4656589fab4a77307a7a71392283b6ca7aa4&code=${code}&grant_type=refresh_token`, (error, response, body) => {
+
+        });
+    } catch (err) {
+        return new Error(err)
+    }
+}
 export const getStraveAPI = async (req, res) => {
     try {
-        // res.writeHeader(200, { "Content-Type": "text/html" });
-        console.log(__dirname)
-        res.sendFile(path.join(__dirname, '/index.html'));
+        const { code } = req.query;
+        const clientss = await clients('Accounts');
+        let client_id, client_secret, refreshToken
+        const existing = await clientss.findOne({ client_id: '128672' }, (err, result) => {
+            if (err) {
+                throw err;
+            };
+            return result;
+        });
+        if (existing) {
+            client_id = result.client_id;
+            client_secret = result.client_secret;
+            refreshToken = result.refreshToken;
+        } else {
+            request.post(`https://www.strava.com/oauth/token?client_id=128672&client_secret=291d4656589fab4a77307a7a71392283b6ca7aa4&code=${code}&grant_type=authorization_code`, async (error, response, body) => {
+                const responseBody = JSON.parse(response.body)
+                if (responseBody.expires_at < Date.now() / 1000) {
+                    //expired
+                    await getRefreshToken('128672', '291d4656589fab4a77307a7a71392283b6ca7aa4', responseBody.refresh_token)
+                } else {
+                    client_id = responseBody.client_id;
+                    client_secret = responseBody.client_secret;
+                    refreshToken = responseBody.refreshToken;
+                }
+
+            });
+        }
+
+        return res.status(200).json(code)
     } catch (err) {
         return res.status(200).json({ success: true, message: "router doesn't works" })
     } finally {
@@ -17,9 +66,6 @@ export const getStraveAPI = async (req, res) => {
 
 export const postData = async (req, res) => {
     try {
-        // const a = await clients();
-        // const collection = a.collection('test');
-        // const doc = { name: 'aha', class: 'statistics' };
         const body = req.body;
         console.log(body)
         const clientss = await clients('test');
--- routes/api/strave.js
+++ routes/api/strave.js
@@ -3,8 +3,6 @@ import { getStraveAPI, postData } from "../../controller/stravaController.js"
 const router = express.Router();
 
 router.get('/apiexchange', async (req, res) => await getStraveAPI(req, res));
-router.post('/insert', async (req, res) => await postData(req, res));
-router.get('/')
 
 
 export default router;
